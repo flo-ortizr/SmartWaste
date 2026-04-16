be50624ebcd5f83bd40b641d13c529f4f1172cbe
unknown <ricardo.andrade@ucb.edu.bo>
2026-04-15 21:32:23 -0400

    feat: conectar la logica de mostrar rutas con la interfaz de usuario

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
 import CrearReporte from "./gestionadorbasura.js";
+import { mostrarRutas } from "./rutas.js";
 
 const formReporte = document.querySelector("#reporte-form");
 const div = document.querySelector("#resultado-div");
 const inputMensaje = document.querySelector("#mensaje_reporte");
 
+const btnVerRutas = document.querySelector("#btn-ver-rutas");
+const divListaRutas = document.querySelector("#lista-rutas-div");
 
 if (formReporte) 
 {
@@ -12,5 +15,15 @@ if (formReporte)
     const mensaje = inputMensaje.value;
     div.innerHTML = CrearReporte(mensaje);
   });
+
+if (btnVerRutas) {
+  btnVerRutas.addEventListener("click", () => {
+    const rutasSimuladas = [
+      { zona: "Zona Norte - Cala Cala", dias: "Lunes, Miércoles y Viernes" },
+      { zona: "Zona Sur - La Chimba", dias: "Martes, Jueves y Sábados" }
+    ];
+    divListaRutas.innerHTML = mostrarRutas(rutasSimuladas);
+  });
+}
 
 }
