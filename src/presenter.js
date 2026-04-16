181b0f245a61b9479d3684464f4617c99cd7f382
User <oriana.flores@ucb.edu.bo>
2026-04-15 22:17:32 -0400

    feat: agregar formulario y presenter para crear rutas

--- src/presenter.js
+++ src/presenter.js
@@ -1,13 +1,32 @@
 import CrearReporte from "./gestionadorbasura.js";
+import crearRuta from "./crearRuta.js";
 
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const formReporte = document.querySelector("#sumar-form");
+const divReporte = document.querySelector("#resultado-div");
 const inputMensaje = document.querySelector("#mensaje_reporte");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+if (formReporte) {
+  formReporte.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  const mensaje = inputMensaje.value;
+    const mensaje = inputMensaje.value;
 
-  div.innerHTML = CrearReporte(mensaje);
-});
+    divReporte.innerHTML = CrearReporte(mensaje);
+  });
+}
+
+const formRuta = document.querySelector("#ruta-form");
+const divRuta = document.querySelector("#resultado-ruta-div");
+const inputNombreRuta = document.querySelector("#nombre_ruta");
+const inputZonaRuta = document.querySelector("#zona_ruta");
+
+if (formRuta) {
+  formRuta.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nombreRuta = inputNombreRuta.value;
+    const zonaRuta = inputZonaRuta.value;
+
+    divRuta.innerHTML = crearRuta(nombreRuta, zonaRuta);
+  });
+}
