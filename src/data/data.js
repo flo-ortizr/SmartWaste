fa92f9e900a82f90e2f5a9524555dd9114eae2c4
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2026-05-12 09:43:24 -0400

    refactor: externalizar datos hardcodeados a data.js

---
+++ src/data/data.js
@@ -0,0 +1,16 @@
+export const zonasCochabamba = [
+  "Cala Cala",
+  "Las Cuadras",
+  "Tupuraya",
+  "Queru Queru",
+  "Alalay",
+  "Sarco",
+  "Pacata",
+  "Villa Busch",
+  "Temporal",
+  "Muyurina"
+];
+
+export const reportesBD = [];
+
+export const rutasBD = [];
